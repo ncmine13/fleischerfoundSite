b1665b98ab9025285474c8dc488c0eb42628d594
Naomi Mine <naomi.mine@gmail.com>
2017-05-22 15:00:41 -0500

    working on contribute page

--- js/app.js
+++ js/app.js
@@ -2,8 +2,33 @@ console.log("hooked up")
 
 $('.parallax-window').parallax();
 
-	$('#nav').affix({
-		offset: {
-			top: $('.jumbotron').height() && $('.header').height()
-		}
-	});
+$('#nav').affix({
+	offset: {
+		top: $('.jumbotron').height() && $('.header').height()
+	}
+});
+
+
+var minimized_elements = $('p.minimize');
+
+minimized_elements.each(function(){    
+    var text = $(this).text();        
+    if(text.length < 100) return;
+    
+    $(this).html(
+        text.slice(0,100)+'<span>... </span><a href="#" class="more">More</a>'+
+        '<span style="display:none;">'+ text.slice(100,text.length)+' <a href="#" class="less">Less</a></span>'
+    );
+    
+}); 
+
+$('a.more', minimized_elements).click(function(event){
+    event.preventDefault();
+    $(this).hide().prev().hide();
+    $(this).next().show();        
+});
+
+$('a.less', minimized_elements).click(function(event){
+    event.preventDefault();
+    $(this).parent().hide().prev().show().prev().show();    
+});
